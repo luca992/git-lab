9553d491ec9f3908570ca0d706886217d07c1feb
Luca Spinazzola <spinazzo@usc.edu>
2013-10-30 18:45:19 -0700

    Added rule for Fizz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,9 +2,9 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		for(int i = 0; i < 100; i++) {
-            System.out.println(i);
+		 for(int i = 0; i < 100; i++) {
+            if( i % 3 == 0)
+                System.out.println(i + ": " + "Fizz");
         }
 	}
 
