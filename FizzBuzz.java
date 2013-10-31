5c55595564ba54465cdeb5773360539700c294ab
Andre Duvoisin <duvoisin@usc.edu>
2013-10-30 18:45:08 -0700

    Added rule for Buzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,11 +1,7 @@
-
 public class FizzBuzz {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		for(int i = 0; i < 100; i++) {
-            System.out.println(i);
-        }
+		for(int i = 0; i < 100; i++)
+           	if( i % 5 == 0)
+          			System.out.println("Buzz");
 	}
-
 }
